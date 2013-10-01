2a31b7d4832a3b24998826bf499b47f1a7b1a274
Randy Morgan <digital.ipseity@gmail.com>
2013-10-02 01:45:55 +0900

    require slim

--- lib/apic/engine.rb
+++ lib/apic/engine.rb
@@ -1,6 +1,7 @@
 module Apic
   class Engine < ::Rails::Engine
     isolate_namespace Apic
+    require 'slim'
     require 'jquery-rails'
     require 'bootstrap-sass'
     ActiveSupport.on_load(:action_controller) do
