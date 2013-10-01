44578e1f6756c8cdfa298f74e4174053c7231046
Randy Morgan <digital.ipseity@gmail.com>
2013-10-02 01:40:58 +0900

    use application controller

--- app/controllers/apic/application_controller.rb
+++ app/controllers/apic/application_controller.rb
@@ -1,4 +1,11 @@
 module Apic
   class ApplicationController < ActionController::Base
+    def index
+      endpoints
+    end
+
+    def endpoints
+      @endpoints ||= Apic.endpoints
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 if Rails.version < '4.0.0'
 
   Apic::Engine.routes.draw do
-    match "/" => "#index"
+    match "/" => "application#index"
   end
 else
 
 
   Apic::Engine.routes.draw do
-    get "/" => "#index"
+    get "/" => "application#index"
   end
 end
