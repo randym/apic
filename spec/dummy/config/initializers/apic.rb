6a3299e528d6ab8363d630d1ece49cae6479001a
Randy Morgan <digital.ipseity@gmail.com>
2013-10-02 23:54:05 +0900

    enable authentication_filter in dummy

--- spec/dummy/config/initializers/apic.rb
+++ spec/dummy/config/initializers/apic.rb
@@ -1,1 +1,10 @@
+# Specify a route matcher to restrict which application routes are available
+# in the console. By default all routes will be loaded
+#
+# Apic.route_matcher = /\api\/v1\//
+#
+#
+# Specify your authentication filter. Requests that use this filter will be marked
+# as restricted in the Api endpoints list.
+#
 Apic.authentication_filter = :authenticate
