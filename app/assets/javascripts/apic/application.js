ac93faf3336f7c41a23caff29197cd221a05cb94
randym <randym@reallyenglish.com>
2014-01-16 07:11:57 +0000

    rename bootstrap and jquery assets to indicate that they are pulled specificially for apic

--- app/assets/javascripts/apic/application.js
+++ app/assets/javascripts/apic/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require apic/jquery/jquery-1.10.2.min.js
-//= require apic/bootstrap/bootstrap.min.js
+
+//= require apic/jquery/apic.jquery-1.10.2.min.js
+//= require apic/bootstrap/apic.bootstrap.min.js
 //= require_tree ./components/
 //= require apic/apic.js
